2f788b990dd0e72f8ff6fe3c07a2455f42ad28f5
LiedsonF <151666827+LiedsonF@users.noreply.github.com>
2024-04-15 17:36:50 +0000

    adiciona pagina inicial

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 
 
 function Home(){
-    return <h1>Eduarda, eu amo você. Se você gosta de mim pelo menos um pouquinho, me manda um 😉</h1>
+    return <h1>Eduarda, eu amo você. Se você gosta de mim pelo menos um pouquinho, me manda um 😄</h1>
 }
 
 export default Home;
