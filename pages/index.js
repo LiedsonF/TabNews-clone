b93e2079475d1de78f13a47fa3e03498c01c87ee
LiedsonF <151666827+LiedsonF@users.noreply.github.com>
2024-04-18 17:49:11 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
-
-
-function Home(){
-    return <h1>Eduarda, eu amo você. Se você gosta de mim pelo menos um pouquinho, me manda um 😄</h1>
+function Home() {
+  return (
+    <h1>
+      Eduarda, eu amo você. Se você gosta de mim pelo menos um pouquinho, me
+      manda um 😄
+    </h1>
+  );
 }
 
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("teste");
+}
 export default Home;
